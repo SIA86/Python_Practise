ea0957ac138a0776d2ff11824662c4f75b3dfdad
Igor Saf <sfron4ik@gmail.com>
2022-07-31 01:10:44 +0300

    lesson2: edit whitespaces and newlines

--- Parsing/lesson2/lesson2.py
+++ Parsing/lesson2/lesson2.py
@@ -3,7 +3,6 @@ Implement a code which will parse multy-data from the https://www.finam.ru/quote
 a CSV file to store the information about stocks (name, last price, volumes)
 """
 
-from unicodedata import name
 import requests
 import csv
 from bs4 import BeautifulSoup 
@@ -19,32 +18,28 @@ def normalize(str):
 
 
 def normalize2(str):
-    return str.replace('&nbsp;', '')
+    return str.replace('\xa0', '')
 
 
 def make_CSV(data):
-    with open("stocks.csv", 'a') as f:
+    with open("stocks.csv", 'a', newline='') as f:
         writer = csv.writer(f)
 
         writer.writerow((data["name"],
-                        data["last_price"],
-                        data["volume"]))
+                         data["last_price"],
+                         data["volume"]))
 
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
     stocks = soup.find("tbody", class_="QuoteTable__tableBody--dHw").find_all("tr")
-    #print(len(stocks))
 
     for stock in stocks:
-        name = stock.find('td', class_="QuoteTable__tableCell--151 QuoteTable__left--3jl").find('a').get('title')
+        name = stock.find('td', class_="QuoteTable__tableCell--151 QuoteTable__left--3jl").find('a').text
         last_price = stock.find_all('td')[2].find('span').text
         last_price = normalize(last_price)
         volume = stock.find_all('td')[8].find('span').text
         volume = normalize2(volume)
-
-        #print(last_price)
-        #print(volume)
             
         data = {"name": name,
                 "last_price": last_price,
