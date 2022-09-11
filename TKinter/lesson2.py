41225e7a98c0fab0343a2572977c70ecbaced32e
Igor Saf <sfron4ik@gmail.com>
2022-09-12 01:12:26 +0300

    Lesson2 complete

--- TKinter/lesson2.py
+++ TKinter/lesson2.py
@@ -1,5 +1,12 @@
 from tkinter import *
 import tkinter.ttk as ttk
 
+
 root = Tk()
+frm = ttk.Frame()
+entry = ttk.Entry(master=frm, background='white', foreground='black', width=40)
+entry.pack()
+entry.insert(0, "What is your name?")
+frm.pack()
+
 root.mainloop()
