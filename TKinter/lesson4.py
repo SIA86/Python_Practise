578e06ec60aa10d5f90a35103a68ac416b1f11f7
Igor Saf <sfron4ik@gmail.com>
2022-09-12 20:41:36 +0300

    Lesso4 coplete

--- TKinter/lesson4.py
+++ TKinter/lesson4.py
@@ -1,25 +1,19 @@
 import tkinter as tk
+from tkinter import ttk
+import random
 
-def increase():
-    value = int(lbl_value["text"])
-    lbl_value["text"] = f"{value + 1}"
-
-def decrease():
-    value = int(lbl_value["text"])
-    lbl_value["text"] = f"{value - 1}"
+def roll():
+    lbl_value["text"] = str(random.randint(1, 6))
 
 window = tk.Tk()
 
-window.rowconfigure(0, minsize=50, weight=1)
-window.columnconfigure([0, 1, 2], minsize=50, weight=1)
+window.rowconfigure([0, 1], minsize=50, weight=1)
+window.columnconfigure(0, minsize=50, weight=1)
 
-btn_decrease = tk.Button(master=window, text="-", command=decrease)
-btn_decrease.grid(row=0, column=0, sticky="nsew")
+btn_roll = ttk.Button(master=window, text="Roll", command=roll)
+btn_roll.grid(row=0, column=0, sticky="nsew")
 
-lbl_value = tk.Label(master=window, text="0")
-lbl_value.grid(row=0, column=1)
-
-btn_increase = tk.Button(master=window, text="+", command=increase)
-btn_increase.grid(row=0, column=2, sticky="nsew")
+lbl_value = ttk.Label(master=window, text="0")
+lbl_value.grid(row=1, column=0)
 
 window.mainloop()
