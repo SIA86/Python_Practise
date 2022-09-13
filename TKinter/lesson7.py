b48fbb1734eb9786cf195abf87f654f6c1ae292a
Igor Saf <sfron4ik@gmail.com>
2022-09-13 12:51:23 +0300

    Lesson7 complete. Icons, image, exit button

--- TKinter/lesson7.py
+++ TKinter/lesson7.py
@@ -1,2 +1,18 @@
 from tkinter import *
 import tkinter.ttk as ttk
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Application with ICO')
+root.iconbitmap('favicon.ico')
+my_img = ImageTk.PhotoImage(Image.open('Coco.jpg'))
+
+button_exit = ttk.Button(root, image=my_img, command=root.quit)
+button_exit.grid(column=0, row=0)
+
+
+img2 = PhotoImage(file='image1.gif')
+lbl2 = ttk.Label(root, image=img2)
+lbl2.grid(column=1, row=0)
+
+root.mainloop()
