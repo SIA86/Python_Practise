ed4b91bad3591cddd403cffd0cc7aab98b4fb63f
Igor Saf <sfron4ik@gmail.com>
2022-09-15 00:28:24 +0300

    10.Slider file complete. Color mixer

--- TKinter/tets.py
+++ TKinter/tets.py
@@ -1,16 +1,4 @@
-import tkinter as tk  
-from tkinter import Label, filedialog   
 from PIL import Image, ImageTk  
 
-root = tk.Tk()
-root.filename = filedialog.askopenfilename(initialdir=r'C:\Users\Igor\Documents\Python', 
-                                           title='some title', 
-                                           filetypes=(('png files', '*.png'),
-                                           ('jpg files', '*.jpg')))
-
-my_img = ImageTk.PhotoImage(Image.open(root.filename))
-lbl1 = tk.Label(root, image=my_img)
-lbl1.pack()
-
-
-root.mainloop()                 
+img = Image.new(mode='RGB', size=(200, 200), color=(0, 255, 255))
+img.show()
